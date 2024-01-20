b0d40ebe46f4038230418cf63997f941641decab
Luksfull <105737001+Luksfull@users.noreply.github.com>
2024-01-20 14:54:52 +0200

    Started developing the app. Atm the app asks the client for a location through a form; after submit, the location passed as input is used as a parameter for an API call to weatherapi.com

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import { Geo } from 'react-bootstrap-icons'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [location, setLocation] = useState('')
+  const [formData, setFormData] = useState('')
+
+  useEffect(() => {
+    fetch(`https://api.weatherapi.com/v1/current.json?key=69c788fac5954f2cb8e94409242001&q=${location}&aqi=no`)
+      .then(res => {
+        if(!res.ok) {
+          throw Error('weather data not available')
+        }
+        return res.json()
+      })
+      .then(data => console.log(data))
+  }, [formData])
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    setFormData(location)
+  }
+
+  const handleChange = e => {
+    setLocation(e.target.value)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label><Geo /> Location <Geo /></Form.Label>
+          <Form.Control 
+            type='text' 
+            placeholder='Enter location' 
+            id='location'
+            name='location'
+            onChange={handleChange}
+          />
+          <Button type='submit'>Submit</Button>
+        </Form.Group>        
+      </Form>
+      <h2>{formData}</h2>
     </>
   )
 }
