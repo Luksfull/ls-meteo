4d4da7d05edb0d3afa4238cdce6bb283aee650e8
Luksfull <105737001+Luksfull@users.noreply.github.com>
2024-02-01 17:46:53 +0200

    Ready for pushing the app containing the new autocomplete search component

---
+++ src/Api.jsx
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ed7be17e95msh0a421fe238ca172p18cfeejsn3b1b81b294da',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
